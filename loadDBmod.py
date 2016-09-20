f2ca9963786d021fa9ca2f9175e394afbc226d9c
Garrett Dancik <garrett.dancik@gmail.com>
2016-09-20 09:49:39 -0400

    Modified getCancerDataCalls.py and loadDBmod.py -- need to unlist authors to get single string and download all data

--- loadDBmod.py
+++ loadDBmod.py
@@ -1,21 +1,22 @@
 from Bio import Entrez
 from Bio import Medline
 
-searchTerm = raw_input("Enter cancer type")
-filename = ArticleInfo.txt
-def getCancerData(searchTerm , filename)
+def getCancerData(searchTerm , filename, email) :
     f = open(filename, "w")
-    Entrez.email = "underwoodmi@my.easternct.edu"     # Always tell NCBI who you are
+    Entrez.email = email     # Always tell NCBI who you are
     handle = Entrez.egquery(term= searchTerm)
     record = Entrez.read(handle)
     for row in record["eGQueryResult"]:
         if row["DbName"]=="pubmed":
             print(row["Count"])         #prints number of articles
 
-    handle = Entrez.esearch(db="pubmed", term = searchTerm, retmax=row["Count"])
+ 
+    retmax = row["Count"]
+    retmax = 200 
+
+    handle = Entrez.esearch(db="pubmed", term = searchTerm, retmax=retmax)
     record = Entrez.read(handle)
     idlist = record["IdList"]
-
 
     handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline",
                            retmode="text")
@@ -23,12 +24,23 @@ def getCancerData(searchTerm , filename)
 
     records = list(records) #all pmids are in this list
 
+    count = 1
     for record in records:
-        f.write("PMID:" , idlist[record])
-        f.write("title:", record.get("TI", "?"))
-        f.write("authors:", record.get("AU", "?"))            #writes the title, author, 
-        f.write("source:", record.get("SO", "?"))           #source and abstract to a file
-        f.write("abstract:", record.get("AB", "?")
+
+	#print record
+
+	print "Article #", count
+	print "PMID: ", record.get("PMID", "?") 
+        print "title:", record.get("TI", "?")
+        print "authors:", record.get("AU", "?")           #writes the title, author, 
+        print "source:", record.get("SO", "?")           #source and abstract to a file
+	
+	count = count + 1
+        f.write("PMID: " + record.get("PMID", "?"))
+        f.write("title: " + record.get("TI", "?"))
+        f.write("authors: " + record.get("AU", "?"))            #writes the title, author, 
+        f.write("source: " + record.get("SO", "?"))           #source and abstract to a file
+        f.write("abstract: " + record.get("AB", "?"))
         
     handle.close()
-f.close()
+    f.close()
