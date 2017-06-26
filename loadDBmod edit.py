0394fdb8bc959d0e9e9988582aaa4755628148a5
Michael Underwood <underwoodmi@my.easternct.edu>
2017-06-26 17:09:33 -0400

    Update loadDBmod edit.py

--- loadDBmod edit.py
+++ loadDBmod edit.py
@@ -1,6 +1,8 @@
 from Bio import Entrez
 from Bio import Medline
 
+# This files extracts information from Pubmed and puts them into files
+
 def getCancerData(searchTerm , filename, email):
         
         Entrez.email = email     # Always tell NCBI who you are
@@ -16,38 +18,38 @@ def getCancerData(searchTerm , filename, email):
 
         
         
-        handle = Entrez.esearch(db="pubmed", term = searchTerm, retmax=retmax)
-        record = Entrez.read(handle)
-        idlistAll = record["IdList"]
+        handle = Entrez.esearch(db="pubmed", term = searchTerm, retmax=retmax)   
+        record = Entrez.read(handle)                                             
+        idlistAll = record["IdList"]                                             
 
-### loop through each batch
+### loop through each batch. There is a limit to efetch 
         start = 0
         
         
         while start < len(idlistAll):
 
-                filename2 = filename + str(start) + ".txt"
-                f = open(filename2, "w")
+                filename2 = filename + str(start) + ".txt"      #Creates file names
+                f = open(filename2, "w")                        #Opens them
                 
                 batchSize = 2000
 
                 end = start + batchSize
                 if end > len(idlistAll) + 1 :
-                        end = len(idlistAll) + 1
+                        end = len(idlistAll) + 1     #Creates the batches
                         
                 idlist = idlistAll[start:end]
                 
                 
-                handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text")
-                records = Medline.parse(handle)
+                handle = Entrez.efetch(db="pubmed", id=idlist, rettype="medline", retmode="text") #
+                records = Medline.parse(handle)                                                   # Extracts the info from pubmed
                 records = list(records) #all pmids are in this list
                 
                 for record in records:	#print record
                   #  print "Article #", count
                   #  print "PMID: ", record.get("PMID", "?") 
                   #  print "title:", record.get("TI", "?")
-                  #  print "authors:", record.get("AU", "?")           #writes the title, author, 
-                  #  print "source:", record.get("SO", "?")           #source and abstract to a file
+                  #  print "authors:", record.get("AU", "?")           
+                  #  print "source:", record.get("SO", "?")           
                   #  count = count + 1
                     s = ", "
                     authors = s.join(record.get("AU", "?"))
@@ -59,7 +61,7 @@ def getCancerData(searchTerm , filename, email):
                      
                 print ("Batch starting at " + str(start) + " is complete")  
                 
-                start = start + batchSize
+                start = start + batchSize    #moves to next batch
         handle.close()
         f.close
 
